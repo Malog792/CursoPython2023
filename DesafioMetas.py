38292f2107b268e4fcc8221bef7e804ec6b4d6fa
Malog792 <116230295+Malog792@users.noreply.github.com>
2023-04-11 22:57:08 +0000

    Adicionei uma segunda meta e fiz as diferenças

--- DesafioMetas.py
+++ DesafioMetas.py
@@ -3,13 +3,21 @@
 #Se a meta for ultrapassada em mais de 500 unidades, os vendedores receberão bonus de 15%.
 
 print("Desafio de Metas:\n Informe o número de vendas realizadas pelos vendedores e será calculada se a meta foi atingida e o bônus que os funcionários recebem\n")
+
 meta1 = 50_000
 meta2 = 50_500
+print("Meta de Vendas: ",meta1)
 vendas = float(input("Número de Vendas: "))
 if vendas < meta1:
-    resto = meta - vendas
-    print("Infelizmente faltou {} para atingir a meta de vendas. O bônus não será repassado. ".format(resto))
-elif vendas >= meta1 and <meta2:
+    resto = meta1 - vendas
+    print("Infelizmente faltou {:.0f} para atingir a meta de vendas. O bônus não será repassado. ".format(resto))
+elif vendas <= meta2:
+  resto = meta2 - vendas
+  print("Muito bom, meta atingida, bônus de 5 %, faça mais {:.0f} venda(s) na próxima para conseguir mais bônus".format(resto))
+elif vendas > meta2:
+  resto = vendas - meta2
+  print("Meus parabéns, vocês superaram em {:.0f} venda(s) a meta de vendas, bônus de 15% para os ótimos funcionários ".format(resto))
+
    
   
   
